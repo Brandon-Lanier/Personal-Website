41fea200f9acc6743f97d1423eb2e8643e9698d6
Brandon Lanier <brandonjlanier@gmail.com>
2022-04-05 09:08:17 -0500

    Updated profile image

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,22 +1,8 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { useState } from 'react';
-import { Link } from '@mui/material';
-import NavLinks from '../NavLinks/NavLinks';
 import Randy from '../images/randyweb2.png'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import Fade from '@mui/material/Fade';
 import './Landing.css'
 
 function Landing() {
